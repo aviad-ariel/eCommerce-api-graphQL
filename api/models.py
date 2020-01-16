58f1b324d5e0cefa17b15c0eae37767ced0796c8
aviad <arielaviad1@gmail.com>
2020-01-17 00:54:06 +0200

    api models created

---
+++ api/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    supplier_name = models.CharField(max_length=50)
+    is_published = models.BooleanField()
+    selling_price = models.FloatField()
+    buying_price = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Collection(models.Model):
+    name = models.CharField(max_length=50)
+    products = models.ManyToManyField('Product', related_name='products')
+    average_products_price = models.FloatField()
+
+    def __str__(self):
+        return self.name
